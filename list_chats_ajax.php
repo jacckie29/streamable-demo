8042a9caac97941a823dbd47d3b8bac7c1c84a10
Janak <merjackie@gmail.com>
2018-03-26 00:57:15 +0530

    Session maintain

--- list_chats_ajax.php
+++ list_chats_ajax.php
@@ -8,10 +8,10 @@ if (isset($_SESSION['timeout'])) {
     $session_life = time() - $_SESSION['timeout'];
     if ($session_life > $inactive) {
         session_destroy();
-        header("Location: index.php");
+        //header("Location: index.php");
+        $htmlBody = "failure";
     }
 }
-$_SESSION['timeout'] = time();
 
 $client = new Google_Client();
 $client->setClientId($OAUTH2_CLIENT_ID);
